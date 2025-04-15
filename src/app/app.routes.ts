4d3f014f6291b1ac819723581564181359a326de
Rocio Reynoso <89603734+ReynosoRocio@users.noreply.github.com>
2025-04-15 10:28:47 -0600

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
+  ];
