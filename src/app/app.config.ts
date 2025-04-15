5ecdbe4fe3be6b6f4a6c49a4e04eeb761b349250
Rocio Reynoso <89603734+ReynosoRocio@users.noreply.github.com>
2025-04-15 11:42:42 -0600

    add ng select

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,15 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http'; // I
 import { routes } from './app.routes';
 import { JwtInterceptor } from './jwt.interceptor';
 
+import { importProvidersFrom } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([JwtInterceptor])) // Uso correcto de withInterceptors
+    provideHttpClient(withInterceptors([JwtInterceptor])), // Uso correcto de withInterceptors
+    importProvidersFrom(NgSelectModule) // Importa NgSelectModule aquí
   ]
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { NgLabelTemplateDirective, NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [NgSelectModule, FormsModule, NgLabelTemplateDirective, NgOptionTemplateDirective, NgSelectComponent], // Importa aquí los módulos que necesites
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   isRightPanelActive = false;
+
+  states : Array<{id:number, name : String}> =  [
+    { id: 1, name: "Aguascalientes" },
+    { id: 2, name: "Campeche" },
+    { id: 3, name: "Chiapas" },
+    { id: 4, name: "Chihuahua" },
+    { id: 5, name: "Coahuila" },
+    { id: 6, name: "Colima" },
+    { id: 7, name: "Durango" },
+    { id: 8, name: "Estado de México" },
+    { id: 9, name: "Guanajuato" },
+    { id: 10, name: "Guerrero" },
+    { id: 11, name: "Hidalgo" },
+    { id: 12, name: "Jalisco" },
+    { id: 13, name: "Michoacán" },
+    { id: 14, name: "Morelos" },
+    { id: 15, name: "Nayarit" },
+    { id: 16, name: "Nuevo León" },
+    { id: 17, name: "Oaxaca" },
+    { id: 18, name: "Puebla" },
+    { id: 19, name: "Querétaro" },
+    { id: 20, name: "Quintana Roo" },
+    { id: 21, name: "San Luis Potosí" },
+    { id: 22, name: "Sinaloa" },
+    { id: 23, name: "Sonora" },
+    { id: 24, name: "Tabasco" },
+    { id: 25, name: "Tamaulipas" },
+    { id: 26, name: "Tlaxcala" },
+    { id: 27, name: "Veracruz" },
+    { id: 28, name: "Yucatán" },
+    { id: 29, name: "Zacatecas" }
+  ];
+
+  selectedState: number | null = null;
 
   onSignUpClick(): void {
     this.isRightPanelActive = true;
@@ -15,4 +54,6 @@ export class LoginComponent {
   onSignInClick(): void {
     this.isRightPanelActive = false;
   }
+
+
 }
