d07e2624224c97c4540c2e9d34401998cb6f1eb8
Rocio Reynoso <89603734+ReynosoRocio@users.noreply.github.com>
2025-04-16 09:25:26 -0600

    save a interceptor

--- src/app/jwt.interceptor.ts
+++ src/app/jwt.interceptor.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
 
-export const JwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('authToken'); // Obtén el token del almacenamiento local
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private _authenticationService: AuthenticationService, @Inject('BASE_URL') private _baseUrl: string) {}
 
-  if (token) {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return next(clonedRequest);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const isLoggedIn = this._authenticationService.isAuthenticated();
+    const isApiUrl = request.url.indexOf(this._baseUrl) === 0;
+
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this._authenticationService.getToken()}`
+        }
+      });
+    }
+
+    return next.handle(request);
   }
-
-  return next(req);
-};
+}
